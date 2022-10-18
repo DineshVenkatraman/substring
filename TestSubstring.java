b3bed316d36f34ab5cc8f87e5db64d6f9efe6c98
Dinesh Venkatraman <114977306+DineshVenkatraman@users.noreply.github.com>
2022-10-18 14:33:55 +0530

    substring
    substring

---
+++ TestSubstring.java
@@ -0,0 +1,8 @@
+public class TestSubstring {
+    public static void main(String args[]){
+        String s="SachinTendulkar";
+        System.out.println("Original String: " + s);
+        System.out.println("Substring starting from index 6: " +s.substring(6));//Tendulkar
+        System.out.println("Substring starting from index 0 to 6: "+s.substring(0,6)); //Sachin
+    }
+}
